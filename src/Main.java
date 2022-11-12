62b8964efc5e9a155eda1ec70938f291e33b1c54
Friday <ariff.norhadi@myfave.com>
2022-11-12 10:17:02 +0800

    tapis input dari user

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello guys!");
+        Scanner input= new Scanner(System.in);    //System.in is a standard input stream
+        System.out.print("Enter first number- ");
+        int user_input = input.nextInt(); // terima input
+
+        // kena terima nombor 1 , 2 dan 3 sahaja
+        // jadinya, selagi user masukkan input selain ini, kita keep
+        // asking them to enter the number
+
+        while (!(user_input == 1 || user_input == 2 || user_input == 3)) {
+            System.out.print("Enter first number- ");
+            user_input = input.nextInt(); // input baru sebab dia salah masuk nombor
+            System.out.println("this is a "+ user_input);
+            if (user_input == 1 || user_input == 2 || user_input == 3) {
+                break;
+            }
+        }
+
+        System.out.println("this is final user input "+ user_input);
+
+
+
     }
 }
